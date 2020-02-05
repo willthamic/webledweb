91307bd85d916eb701c1308936af73df4383d3be
willthamic <will.hamic@gmail.com>
2020-02-05 01:48:08 -0600

    update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var config = {
-    apiKey: "AIzaSyAzkuVWvV79TILB1ZlrK7foRKlrHksPsNE",
-    authDomain: "careml.firebaseapp.com",
-    databaseURL: "https://careml.firebaseio.com",
-    projectId: "careml",
-    storageBucket: "careml.appspot.com",
-    messagingSenderId: "893046241267"
+    // apiKey: "AIzaSyAzkuVWvV79TILB1ZlrK7foRKlrHksPsNE",
+    // authDomain: "careml.firebaseapp.com",
+    databaseURL: "webled-2d7e0.firebaseio.com/",
+    projectId: "webled-2d7e0",
+    // storageBucket: "careml.appspot.com",
+    // messagingSenderId: "893046241267"
 };
 firebase.initializeApp(config);
 
@@ -16,6 +16,15 @@ var scale = 5;
 var roomx = 186;
 var roomy = 98;
 
+var NUM_LEDS = 100;
+
+var colorName = [ "aliceblue", "amethyst", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanchedalmond", "blue", "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgreen", "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "gray", "green", "greenyellow", "honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgreen", "lightgrey", "lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plaid", "plum", "powderblue", "purple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "white", "whitesmoke", "yellow", "yellowgreen" ];
+var currentColor = "lightgray";
+
+var state = "color";
+var brightness = 1;
+var ledspeed = 1;
+
 room.setAttribute('style', 'width: ' + roomx * scale + 'px; height: ' + roomy * scale + 'px;')
 
 for (var i = 0; i < 100; i++) {
@@ -25,6 +34,8 @@ for (var i = 0; i < 100; i++) {
     b.setAttribute('style', 'left: ' + a.x + 'px; bottom: ' + a.y + 'px;');
     leds.push(b);
 }
+
+setInterval(ledCallback, 20);
 
 function getLoc(i) {
     var a = {x: 0, y: 0};
@@ -61,15 +72,50 @@ function getLocScale(i) {
 function runScript(e) {
     if (e.keyCode == 13) {
         var tb = document.getElementById("scriptBox");
-        tb.value = '';
+        
+        var msg = tb.value.toLowerCase();
+        if (msg.startsWith("set lights to ")) {
+            msg = msg.substring(14);
+            msg = msg.replace(/\s/g, '');
 
-        const Http = new XMLHttpRequest();
-        const url='https://webled-2d7e0.firebaseio.com/data';
-        Http.open("GET", url);
-        Http.send();
+            processMsg(msg);
 
-        Http.onreadystatechange = (e) => {
-            console.log(Http.responseText)
+            firebase.database().ref('data').set({
+                current: msg,
+            });
+
+            tb.value = '';
         }
     }
 }
+
+function processMsg (msg) {
+    if (colorName.includes(msg)) {
+        state = "color";
+        currentColor = msg;
+    }
+    if (msg == "rainbow")
+        state = "rainbow";
+    else if (msg == "max" || msg == "maximum") {
+        brightness = 1;
+    }
+}
+
+function ledCallback() {
+    var t = new Date();
+    // var t = d.getMilliseconds();
+    if (state == "color") {
+        for (var i = 0; i < NUM_LEDS; i++) {
+            $("#led-" + i).css('background-color', currentColor);
+        }
+    } else if (state == "rainbow") {
+        for (var i = 0; i < NUM_LEDS; i++) {
+            var hue = ((i * 4 + ledspeed * t * .03) / 256 * 360) % 360;
+            hue = Math.floor(hue);
+            $("#led-" + i).css('background-color', 'hsl(' + hue + ',100%,50%)');
+        }
+    }
+    // for (var i = 0; i < NUM_LEDS; i++) {
+    //     leds[i] *= brightness;
+    // }
+} // End of timerCallback 
